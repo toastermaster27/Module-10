782d7bb98a7a2036ee62f402da2805e6d6607dee
toastermaster27 <118187528+toastermaster27@users.noreply.github.com>
2023-02-26 13:40:48 -0500

    ALL THE TESTS PASSED FINALLY

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,28 +1,28 @@
-const employee = require('../lib/employee');
+const Employee = require('../lib/employee');
 
 test('create employee obj', () => {
-    const employee = new employee('Alice', 6, 'alice20@gmail.com');
+    const employee = new Employee('Alice', 6, 'alice20@gmail.com');
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
 
 test('get employee name', () => {
-    const employee = new employee('Alice', 6, 'alice20@gmail.com');
+    const employee = new Employee('Alice', 6, 'alice20@gmail.com');
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
-test('get employee id', () => {
-    const employee = new employee('Alice', 6, 'alice20@gmail.com');
-    expect(employee.getID()).toEqual(expect.any(Number));
-}); 
+test('gets employee ID', () => {
+    const employee = new Employee('Alice', 6, 'alice20@gmail.com');
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
 
 test('get employee email', () => {
-    const employee = new employee('Alice', 6, 'alice20@gmail.com');
+    const employee = new Employee('Alice', 6, 'alice20@gmail.com');
     expect(employee.getEmail()).toEqual(expect.any(String));
 });
 
 test('get employee role', () => {
-    const employee = new employee('Alice', 6, 'alice20@gmail.com');
+    const employee = new Employee('Alice', 6, 'alice20@gmail.com');
     expect(employee.getRole()).toEqual("Employee");
 });
--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -1,17 +1,17 @@
-const intern = require('../lib/intern');
+const Intern = require('../lib/intern');
 
 test('create intern obj', () => {
-    const intern = new intern('Alice', 6, 'alice20@gmail.com', 'SWMU');
+    const intern = new Intern('Alice', 6, 'alice20@gmail.com', 'SWMU');
     expect(intern.school).toEqual(expect.any(String));
 });
 
 test('get employee school', () => {
-    const intern = new intern('Alice', 6, 'alice20@gmail.com', 'SWMU');
+    const intern = new Intern('Alice', 6, 'alice20@gmail.com', 'SWMU');
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
 
 test('get employee role', () => {
-    const intern = new intern('Alice', 6, 'alice20@gmail.com', 'SWMU');
-    expect(intern.getRle()).toEqual("Intern");
+    const intern = new Intern('Alice', 6, 'alice20@gmail.com', 'SWMU');
+    expect(intern.getRole()).toEqual("Intern");
 });
 
